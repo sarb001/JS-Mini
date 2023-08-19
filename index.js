b358dc66eef5518a6f60582bf7e2df93da5784c6
SARB <mrsinghbusiness05@gmail.com>
2023-08-19 18:59:08 +0530

    close btn added -

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 const button = document.getElementById('dialogbox');
 const dialogtext = document.getElementById('dialogtext');
+const closebtn = document.getElementById('close');
 
 function showbox(){
     dialogtext.classList.toggle('open');
 }
 
+function closebox(){
+    dialogtext.classList.remove('open');
+}
+
 button.addEventListener('click',showbox);
+closebtn.addEventListener('click',closebox);
+
 
 
