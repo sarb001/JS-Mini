3992addcc53445f3f20cd34e470f617e5222d259
SARB <mrsinghbusiness05@gmail.com>
2023-08-19 11:44:43 +0530

    dark-mode working -part-5

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 
    const button  = document.getElementById('btn');
-   const body = document.body;
+   const container  = document.getElementById('container');
+
+    let curretmode  = 'lightmode';
+
+    function togglefunction(){
+          if(curretmode === 'lightmode'){
+              container.classList.remove('lightmode');
+             container.classList.add('darkmode');
+             curretmode = 'darkmode';
+          }else if(curretmode === 'darkmode'){
+            container.classList.remove('darkmode');
+            container.classList.add('lightmode');
+            curretmode = 'lightmode';
+          }
+          button.textContent = `Toggle ${curretmode}`
+    }
+
+   button.addEventListener('click' , togglefunction);
    
