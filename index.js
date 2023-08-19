9b9f84861e926af93fe0a7608cc2f22b05d215ce
SARB <mrsinghbusiness05@gmail.com>
2023-08-19 17:58:13 +0530

    Accordian - js-part

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
+// Get all accordion items
+const accordionItems = document.querySelectorAll('.accordion-item');
 
-const container = document.getElementById('container');
+// Attach click event listener to each accordion header
 
-const checkbox  = document.getElementById('check');
-const redbox    = document.getElementById('red');
+ accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.accordion-header');
+  const accordionButton = item.querySelector('.accordion-button');
+  const accordionPanel  =  item.querySelector('.accordion-panel');
+  const accordionArrow  =  item.querySelector('.accordion-arrow');
 
-const text      = document.getElementById('text');
+  // Toggle the open class on click
+  accordionHeader.addEventListener('click', () => {
+    const isOpen = accordionPanel.classList.contains('open');
 
+    // Close all other accordion items
+    accordionItems.forEach((item) => {
+      item.querySelector('.accordion-panel').classList.remove('open');
+      item.querySelector('.accordion-arrow').classList.remove('open');
+    });
 
-function checkboxfunction(){
-       if(!checkbox.checked){
-             container.classList.add('darkmode')
-             container.classList.remove('red');
-        }else{
-            container.classList.remove('darkmode');
-            container.classList.add('red');
-        }
-}
-
-checkbox.addEventListener('change',checkboxfunction);
-
-function redboxfunction(){
-    if(!redbox.checked){
-        container.classList.add('darkmode');
-        container.classList.remove('red');
-    }else{
-        container.classList.remove('darkmode');
-        container.classList.add('red');
+    if (!isOpen) {
+      accordionPanel.classList.add('open');
+      accordionArrow.classList.add('open');
+    } else {
+      accordionPanel.classList.remove('open');
+      accordionArrow.classList.remove('open');
     }
-}
-
-redbox.addEventListener('change', redboxfunction);
-
+  });
+});
