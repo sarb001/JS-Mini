53790afd35e0111ff55df28a0c7fc033eecf0277
SARB <mrsinghbusiness05@gmail.com>
2023-08-19 11:25:44 +0530

    advanced dark-mode

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 
    const button  = document.getElementById('btn');
    const body = document.body;
-
-     function togglecolor(){
-      body.classList.toggle('darkmode');
-      const showtxt =  body.classList.contains('darkmode') ? 'Light-Mode' : 'Dark-Mode';
-      button.innerText = `Toggle ${showtxt}`;
-     }
-
-     button.addEventListener('click',togglecolor);
-
+   
