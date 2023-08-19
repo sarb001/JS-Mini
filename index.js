d72628c6fda35143c67cc690c78c3f1d0a55f122
SARB <mrsinghbusiness05@gmail.com>
2023-08-19 08:56:38 +0530

    darkmode -working -part-4

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
 
-const stringdata    = document.getElementById('stringtyping');
+   const button  = document.getElementById('btn');
+   const body = document.body;
 
-const lowercase     = document.getElementById('lowercase');
-const uppercase     = document.getElementById('uppercase');
-const camelcase     = document.getElementById('camelcase');         // hereInTheCase 
-const pascalcase    = document.getElementById('pascalcase');        // DeadSeaInTheFort
-const snakecase     = document.getElementById('snakecase');
-const kebabcase     = document.getElementById('kebabcase');
-const titlecase     = document.getElementById('titlecase');
-const trimcase      = document.getElementById('trimcase');
+     function togglecolor(){
+      body.classList.toggle('darkmode');
+      const showtxt =  body.classList.contains('darkmode') ? 'Light-Mode' : 'Dark-Mode';
+      button.innerText = `Toggle ${showtxt}`;
+     }
 
-
-function stringtyping(){
-
-    maindata = stringdata.value.trim();     // without trim extra spaces gives Error -- undefined 
-    lowercase.textContent = maindata;
-    trimcase.textContent  = maindata.split(' ').join('');
-
-    camelcase.textContent  =  maindata.split(' ').reduce((a,b) => a+b[0] ?.toUpperCase() + b.substring(1)?.toLowerCase());
-    
-    // Pascal Case - Make first letter capital
-    pascalcase.textContent =  camelcase ?  camelcase.textContent[0].toUpperCase() + camelcase.textContent.substring(1) : '';
-
-    lowercase.textContent = maindata.toLowerCase();
-    uppercase.textContent = maindata.toUpperCase();
-
-    snakecase.textContent = maindata.split(' ').join('_');
-    kebabcase.textContent = maindata.split(' ').join('-');
-    
-}
-
-stringdata.addEventListener('input',stringtyping) 
+     button.addEventListener('click',togglecolor);
 
