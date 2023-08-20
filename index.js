c47a6a36a480ac4c90da4dcd76219050303d1bf6
SARB <mrsinghbusiness05@gmail.com>
2023-08-20 05:48:01 +0530

    toast-popup -getting value

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 
-const button = document.getElementById('dialogbox');
-const dialogtext = document.getElementById('dialogtext');
-const closebtn = document.getElementById('close');
+const toastbutton = document.getElementById('toast');
+const DurationBar = document.getElementById('range');
 
-function showbox(){
-    dialogtext.classList.toggle('open');
+const Horizontal = document.getElementById('horizontal-position');
+const Vertical = document.getElementById('vertical-position');
+const Type     = document.getElementById('type');
+
+
+const showmsgleft   = document.getElementById('showleft');
+const showmsgright  = document.getElementById('showright');
+const showmsgtop    = document.getElementById('showtop');
+const showmsgbottom = document.getElementById('showbottom');
+
+const Toastmessage = document.getElementById('toastmessage');
+const closeToast   = document.getElementById('closetoast');
+
+
+const EnterMessage   = document.getElementById('message');
+const ShowToast = document.getElementById('showtoast');
+
+function showtoast(){
+     Hori = Horizontal.value;
+     console.log(' Hori -',Hori);
+     Verticalvalue = Vertical.value;
+     console.log(' verti -',Verticalvalue);
+     Typevalue = Type.value;
+     console.log(' type -',Typevalue);
 }
 
-function closebox(){
-    dialogtext.classList.remove('open');
-}
+ShowToast.addEventListener('click', showtoast)
 
-button.addEventListener('click',showbox);
-closebtn.addEventListener('click',closebox);
+
+
+
+
+
 
 
 
